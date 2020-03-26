cd7a39ab1845ec1be9a58f33496c9f3d5c74c7ea
Hadrien <hadrienmarcellin@gmail.com>
2020-03-26 18:10:26 +0100

    dao files

--- app/src/main/java/com/example/pillsreminder/dao/PainDao.java
+++ app/src/main/java/com/example/pillsreminder/dao/PainDao.java
@@ -1,4 +1,34 @@
 package com.example.pillsreminder.dao;
 
-public class PainDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.pillsreminder.entities.Pain;
+
+import java.util.List;
+
+@Dao
+public interface PainDao {
+    @Query("SELECT * FROM painTable")
+    LiveData<List<Pain>> getAllPains();
+
+    @Query("SELECt * FROM painTable WHERE id = :id LIMIT 1")
+    LiveData<List<Pain>> findPainById(long id);
+
+    @Query("DELETE FROM painTable")
+    void deleteAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Pain pain);
+
+    @Update
+    void updatePain(Pain pain);
+
+    @Delete
+    void delete(Pain pain);
 }
--- app/src/main/java/com/example/pillsreminder/entities/Pain.java
+++ app/src/main/java/com/example/pillsreminder/entities/Pain.java
@@ -1,6 +1,50 @@
 package com.example.pillsreminder.entities;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 import androidx.room.RoomDatabase;
 
+
+@Entity(tableName = "painTable")
 public class Pain {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "level")
+    private String level;
+
+    @ColumnInfo(name = "long_duration")
+    private boolean long_duration;
+
+    public Pain(int id, String level, boolean long_duration) {
+        this.id = id;
+        this.level = level;
+        this.long_duration = long_duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public boolean isLong_duration() {
+        return long_duration;
+    }
+
+    public void setLong_duration(boolean long_duration) {
+        this.long_duration = long_duration;
+    }
 }
